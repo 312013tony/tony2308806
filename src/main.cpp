424aeab2067b75c000f5d9dd181de35dba4f7feb
Genshin Impact <11312013@st.hhvs.tn.edu.tw>
2025-12-02 14:53:30 +0800

    重構 main.cpp，清理不必要的代碼並增強註解，改善可讀性

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,32 @@
-#include <Arduino.h>
+// ESP32 WiFi Scanner - SSID only
+// 每 10 秒掃描一次，僅在序列埠列出每個 SSID
 
-// put function declarations here:
-int myFunction(int, int);
+#include <Arduino.h>
+#include <WiFi.h>
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+	Serial.begin(9600);
+	delay(1000);
+	Serial.println();
+	Serial.println("ESP32 WiFi Scanner - SSID only");
+
+	WiFi.mode(WIFI_STA);
+	WiFi.disconnect();
+	delay(100);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
+	Serial.println("掃描 SSID...");
+	int n = WiFi.scanNetworks();
+	if (n <= 0) {
+		Serial.println("未找到任何網路");
+	} else {
+		Serial.printf("共找到 %d 個網路:\n", n);
+		for (int i = 0; i < n; ++i) {
+			Serial.printf("%d: %s\n", i + 1, WiFi.SSID(i).c_str());
+		}
+	}
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+	WiFi.scanDelete();
+	delay(10000);
 }
